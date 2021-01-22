dab690c4c262e829975c7bbec7e0e6abb49df417
Marinho <marinho123>
2021-01-22 22:10:53 +0000

    Fix Server

--- routes/generatePDF.js
+++ routes/generatePDF.js
@@ -10,79 +10,79 @@ const readFile = utils.promisify(fs.readFile)
 
 router.get('/', async function (req, res, next) {
 
-    res.send('teste');
     // let modelo = req.body.modelo;
 
-    // let data = {
-    //     nome: 'RUI'
-    // };
-    //
-    // getTemplateHtml('teste')
-    //     .then(async (resp) => {
-    //         // Now we have the html code of our template in res object
-    //         // you can check by logging it on console
-    //         // console.log(res)
-    //
-    //         console.log("Compiing the template with handlebars")
-    //         const template = hb.compile(resp, { strict: true });
-    //         // we have compile our code with handlebars
-    //         const result = template(data);
-    //         // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
-    //         const html = result;
-    //
-    //         // we are using headless mode
-    //         const browser = await puppeteer.launch({
-    //             args: [
-    //                 '--no-sandbox',
-    //                 '--disable-setuid-sandbox'
-    //             ]
-    //         });
-    //         const page = await browser.newPage()
-    //
-    //         // We set the page content as the generated html by handlebars
-    //         await page.setContent(html)
-    //
-    //         // await page.addStyleTag({
-    //         //     content: `
-    //         //     @page:first { margin-top: 0px; margin-bottom: 50px;}
-    //         // `,
-    //         // });
-    //
-    //         // we Use pdf function to generate the pdf in the same folder as this file.
-    //         await page.pdf( {
-    //             path: 'teste.pdf',
-    //             format: 'A4',
-    //             printBackground: true,
-    //             displayHeaderFooter: false,
-    //             margin: {
-    //                 top: 0,
-    //                 bottom: 0,
-    //                 right: 0,
-    //                 left: 0,
-    //             },
-    //         })
-    //         // const contents = await fs.readFile('invoice.pdf', {encoding: 'base64'});
-    //         const contents = await base64_encode('teste.pdf');
-    //
-    //         await browser.close();
-    //         console.log("PDF Generated")
-    //
-    //         // console.log(contents);
-    //
-    //         fs.unlink('teste.pdf', (err) => {
-    //             if (err) {
-    //                 console.error(err)
-    //                 return
-    //             }
-    //             console.log("PDF Removed");
-    //         })
-    //
-    //         res.send(contents);
-    //
-    //     })
-    //     .catch(err => {
-    //         console.error(err)
-    //     });
+    let data = {
+        nome: 'RUI'
+    };
+
+    getTemplateHtml('teste')
+        .then(async (resp) => {
+            // Now we have the html code of our template in res object
+            // you can check by logging it on console
+            // console.log(res)
+
+            console.log("Compiing the template with handlebars")
+            const template = hb.compile(resp, { strict: true });
+            // we have compile our code with handlebars
+            const result = template(data);
+            // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
+            const html = result;
+
+            // we are using headless mode
+            const browser = await puppeteer.launch({
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox'
+                ]
+            });
+            const page = await browser.newPage()
+
+            // We set the page content as the generated html by handlebars
+            await page.setContent(html)
+
+            // await page.addStyleTag({
+            //     content: `
+            //     @page:first { margin-top: 0px; margin-bottom: 50px;}
+            // `,
+            // });
+
+            // we Use pdf function to generate the pdf in the same folder as this file.
+            await page.pdf( {
+                path: 'teste.pdf',
+                format: 'A4',
+                printBackground: true,
+                displayHeaderFooter: false,
+                margin: {
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                },
+            })
+            // const contents = await fs.readFile('invoice.pdf', {encoding: 'base64'});
+            const contents = await base64_encode('teste.pdf');
+
+            await browser.close();
+            console.log("PDF Generated")
+
+            // console.log(contents);
+
+            fs.unlink('teste.pdf', (err) => {
+                if (err) {
+                    console.error(err)
+                    return
+                }
+                console.log("PDF Removed");
+            })
+
+            // res.send(contents);
+            res.send('teste');
+
+        })
+        .catch(err => {
+            console.error(err)
+        });
 
 });
 
