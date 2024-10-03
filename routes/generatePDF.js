d3aff9f9f1d593a7e5ccb56b7ef1a1037b524f4a
Marinho <ruijorgemm@hotmail.com>
2024-10-03 22:03:25 +0100

    deploy without docker

--- routes/generatePDF.js
+++ routes/generatePDF.js
@@ -178,21 +178,21 @@ function createRouter() {
                 // resp.send(html);
 
                 // Running without docker
-                // const chromeOptions = {
-                //     headless: true,
-                //     defaultViewport: null,
-                //     args: [
-                //         '--no-sandbox',
-                //         '--disable-setuid-sandbox',
-                //     ]
-                // }
-                // Running with docker
                 const chromeOptions = {
-                    executablePath: '/usr/bin/google-chrome',
-                    headless: 'new',
-                    ignoreDefaultArgs: ['--disable-extensions'],
-                    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-                };
+                    headless: true,
+                    defaultViewport: null,
+                    args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                    ]
+                }
+                // Running with docker
+                // const chromeOptions = {
+                //     executablePath: '/usr/bin/google-chrome',
+                //     headless: 'new',
+                //     ignoreDefaultArgs: ['--disable-extensions'],
+                //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                // };
 
                 // we are using headless mode
                 const browser = await puppeteer.launch(chromeOptions);
